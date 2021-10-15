564e0b96b444713e5b7c79659cfc380fde83c360
shivendra885 <89910271+shivendra885@users.noreply.github.com>
2021-10-15 17:35:43 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,79 @@
+require('dotenv').config();
+const express = require("express"); 
+const bodyParser = require("body-parser"); 
+const ejs = require("ejs");  
+const mongoose = require("mongoose");   
+const md5 = require("md5"); 
+// const bcrypt = require("bcrypt"); 
+const saltRounds = 10;
+
+const app = express(); 
+
+// console.log(process.env.API_KEY);
+
+app.use(express.static("public")); 
+app.set('view engine','ejs');  
+app.use(bodyParser.urlencoded({
+    extended: true
+}));     
+
+mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true}); 
+
+const userSchema = new mongoose.Schema ({
+    email: String, 
+    password: String
+});
+
+
+const User = new mongoose .model("User", userSchema);  
+
+app.get("/", function(req,res){
+    res.render("home")  
+});
+app.get("/register", function(req,res){  
+    res.render("register")
+});
+app.get("/login", function(req,res){
+    res.render("login")
+});
+   
+app.post("/register", function(req, res){  
+    // bcrypt.hash(req.body.password, saltRounds, function(err, hash){
+        const newUser = new User({
+            email: req.body.username, 
+            password: md5(req.body.password)
+        });
+    
+        newUser.save(function(err){
+            if (err){
+                console.log(err); 
+            } else {
+                res.render("secrets"); 
+            } 
+    });
+                         
+ 
+    });
+// });     
+
+app.post("/login", function(req, res){
+    const username = req.body.username; 
+    const password = md5(req.body.password);    
+
+    User.findOne({email: username}, function(err, foundUser){
+        if (err) {
+            console.log(err); 
+        } else{
+            if(foundUser) {
+                if(foundUser.password === password){
+                    res.render("secrets"); 
+                }
+            }
+        }
+    });
+
+});
+ 
+app.listen(7000, function() {
+    console.log("server started on port 7000.")
+});
